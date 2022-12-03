ed8f259bdb142136ff804469da711c4fbf6c765a
Maxi-Max10 <maxgiu3@gmail.com>
2022-12-03 20:09:44 -0300

    login

--- login.php
+++ login.php
@@ -3,36 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=<form class="form form--hidden--dos" id="restaurar">
-    <link rel="stylesheet" href="login.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Includes/css/estilo_login.css">
     
     <title>Login</title>
 </head>
 <body>
     <div class="container">
-        <form class="form " id="formulario">
+        <form class="form " id="formulario" action="Controller/loginControl.php" method="POST">
             <h1 class="form__title">Iniciar Sesión</h1>
           
             <div class="form-control">
-                <input type="text" id="nombre" class="form__input" autofocus placeholder="Nombre">
+                <input type="text" name="nombre" id="nombre" class="form__input" autofocus placeholder="Nombre">
                 
-                <p></p>
             </div>
             
             <div class="form-control">
-                <input id="contrasenia" type="password" class="form__input" autofocus placeholder="Contraseña">
-               
-                <p></p>
+                <input id="password" name="password" type="password" class="form__input" autofocus placeholder="Contraseña">
             </div>
             
             <button class="form__button" type="submit">Continuar</button>
-            <p class="form__text">
+        </form>
+        <p class="form__text">
                 <a href="restaurar-contraseña.php" class="form__link">Olvidaste tu contraseña?</a>
             </p>
             <p class="form__text">
                 <a class="form__link" href="vista/crearcuenta.php" id="linkLogin">No tienes una cuenta? Crear Cuenta</a>
             </p>
-        </form>
     </div>
    
 </body>
