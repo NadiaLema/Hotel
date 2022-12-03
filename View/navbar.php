606949febc5f8c61620acb50b4426faaff4264b0
Maxi-Max10 <maxgiu3@gmail.com>
2022-12-03 18:24:55 -0300

    files

---
+++ View/navbar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=h, initial-scale=1.0">
+   <link rel="stylesheet" href="./Includes/css/index.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    
+    
+    <title>Hotel Mendoza</title>
+</head>
+<body>
+    <header>
+        
+		<div class="logo">
+            <img src="../Includes/img/log-PhotoRoom.png"  class="logo"  rel="stylesheet" alt=""> 
+        </div>
+       
+		<nav>
+			<ul>
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Nosotros</a></li>
+				<li><a href="#">Habitaciones</a></li>
+				<li><a href="#">Servicios</a></li>
+				<li><a href="#">Contacto</a></li>
+                <li><a href="index.html" class="las la-user-tie" style="font-size:16px" >Iniciar Sesión</a></li>
+			</ul>
+		</nav>
