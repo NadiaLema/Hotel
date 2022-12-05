1402e32f79013196abbb007a21bd4cd402b1057d
= <=>
2022-12-05 00:06:26 -0300

    iwu

--- View/nav2.php
+++ View/nav2.php
@@ -6,28 +6,29 @@
     <meta name="viewport" content="width=h, initial-scale=1.0">
    <link rel="stylesheet" href="./Includes/css/index.css">
    <link rel="stylesheet" href="./Includes/css/habitacion.css">
-<<<<<<< HEAD
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-   
-=======
+   <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
    <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     
->>>>>>> 5623d4bdc3ba714825d5c70591c219883190810b
     <title>Hotel Mendoza</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <a class="navbar-brand" href="#">
-            <img src="../Includes/img/logo.blanco.png" width="100" height="100" alt="">
+            <img src="./Includes/img/logo.blanco.png" width="100" height="100" alt="">
         </a>
         
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            
-          </ul>
+              <ul class="nav nav-pills flex-column">
+        
+        <li class="nav-item text-light">
+          <a class="nav-link" href="#">Volver al Inicio</a>
+        </li>
+        
+      </ul>
+
           
         </div>
       </nav>
