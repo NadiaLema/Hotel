09470633c0a96265830d12f9aa11e7e113f22ef5
= <=>
2022-12-07 13:51:54 -0300

    adminfunci

--- admin/View/parte_inferior.php
+++ admin/View/parte_inferior.php
@@ -1,18 +1,31 @@
 
-    <!-- Essential javascripts for application to work-->
-    <script src="licencia/js/jquery-3.3.1.min.js"></script>
-    <script src="licencia/js/popper.min.js"></script>
-    <script src="licencia/js/bootstrap.min.js"></script>
-    <script src="licencia/js/main.js"></script>
-    <!-- The javascript plugin to display page loading on top-->
-    <script src="licencia/js/plugins/pace.min.js"></script>
-    <!-- Page specific javascripts-->
-	<!-- datatables JS -->
-	
+                <div class="row justify-content-center align-content-center text-center">
+                    <div class="columna col-lg-6">
+                        CONTENIDO
+                    </div>
+
+                </div>
+
+            </main>
+        </div>
+    </div>
+
+
+      
+    <!-- jQuery, Popper.js, Bootstrap JS -->
+    
+    <script src="licencia/jquery/jquery-3.6.1.min.js"></script>
+    <script src="licencia/popper/popper.min.js"></script>
+    <script src="licencia/bootstrap/js/bootstrap.min.js"></script>
+      
+    <!-- datatables JS -->
     <script type="text/javascript" src="licencia/datatables/datatables.min.js"></script>    
      
-    <script type="text/javascript" src="cliente.js"></script>  
-    
    
-  </body>
+    
+
+  
+    
+</body>
+
 </html>
--- admin/habitacion.php
+++ admin/habitacion.php
@@ -1,12 +1,13 @@
 <?php include "View/navbar.php"?>
-<main class="app-content">
-    <div class="app-title">
-         <div>
-            <h1><i class="fa fa-dashboard"></i> Administrador</h1>
-        </div>
-    </div>
-   <!--botón-->
-    <div class="container">
+
+<div class="app-title" >
+    <div>
+    <h1><i class="fa fa-dashboard"></i> Habitación</h1>
+     </div>
+</div>
+
+<!--estructura de la tabla-->
+<div class="container">
         <div class="row">
             <div class="col-lg-12">            
             <button id="btnNuevo" type="button" class="btn btn-info" data-toggle="modal">Nuevo</button>    
@@ -14,33 +15,104 @@
         </div>    
     </div>    
     <br>  
-    
-    <!--tabla-->
+
     <div class="container caja">
         <div class="row">
             <div class="col-lg-12">
             <div class="table-responsive">        
-            <table id="tablahab" class="table table-striped table-bordered table-condensed" style="width:100%" >
+                <table id="tablacliente" class="table table-striped table-bordered table-condensed" style="width:100%" >
                     <thead class="text-center">
                         <tr>
-                            <th>idHabitación</th>
-                            <th>Nombre</th>                   
-                            <th>Descripción</th>  
-                            <th>Precio</th>
-                            <th>Imagen</th>
-                            <th>Acciones</th> 
+                            <th>idHabitaci</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>                                
+                            <th>Dirección</th>  
+                            <th>Provincia</th>
+                            <th>Pais</th>
+                            <th>Telefono</th>
+                            <th>E-mail</th>
+                            <th>Acciones</th>
                         </tr>
                     </thead>
                     <tbody>                           
                     </tbody>        
-                </table>                  
-                             
+                </table>               
             </div>
             </div>
         </div>  
     </div>   
 
+<!--Modal para CRUD-->
+<div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <form id="formcliente">    
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                    <div class="form-group">
+                    <label for="" class="col-form-label">Nombre:</label>
+                    <input type="text" class="form-control" id="nombre">
+                    </div>
+                    </div>
+                    <div class="col-lg-6">
+                    <div class="form-group">
+                    <label for="" class="col-form-label">Apellido</label>
+                    <input type="text" class="form-control" id="apellido">
+                    </div> 
+                    </div>    
+                </div>
+                <div class="row"> 
+                    <div class="col-lg-6">
+                    <div class="form-group">
+                    <label for="" class="col-form-label">Dirección</label>
+                    <input type="text" class="form-control" id="direccion">
+                    </div>               
+                    </div>
+                    <div class="col-lg-6">
+                    <div class="form-group">
+                    <label for="" class="col-form-label">Provincia</label>
+                    <input type="text" class="form-control" id="provincia">
+                    </div>
+                    </div>  
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                        <label for="" class="col-form-label">Pais</label>
+                        <input type="text" class="form-control" id="pais">
+                        </div>
+                    </div>    
+                    <div class="col-lg-6">    
+                        <div class="form-group">
+                        <label for="" class="col-form-label">Teléfono</label>
+                        <input type="text" class="form-control" id="telefono">
+                        </div>            
+                    </div>    
+                </div>     
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                        <label for="" class="col-form-label">E-mail</label>
+                        <input type="text" class="form-control" id="email">
+                        </div>
+                    </div>
 
+                </div>           
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
+                <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
+            </div>
+        </form>    
+        </div>
+    </div>
+</div>  
+      
 
-</main>
 <?php include "View/parte_inferior.php"?>
