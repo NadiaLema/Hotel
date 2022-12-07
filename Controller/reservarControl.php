c432e8fa91d02ca8db8b776eb0a62fdbb6da361d
Maxi-Max10 <maxgiu3@gmail.com>
2022-12-07 19:04:23 -0300

    reserva final

--- Controller/reservarControl.php
+++ Controller/reservarControl.php
@@ -2,16 +2,54 @@
 
     include "../Model/conexion_bd.php";
     $mensaje = null;
+    $resultado = null;
+    $idCliente = null;
+    $idreserva = null;
+
+    if (isset($_POST["ajaxF"]) && isset($_POST["idhabitacion"])) {
+           
+        //$idreserva = $_POST["idreserva"];
+        //$fechaE = $_POST['fechaE'];
+        //$fechaS = $_POST['fechaS'];
+
+        $fecha_ingreso = $_POST['fecha_ingreso'];
+        $fecha_salida  = $_POST['fecha_salida'];
+        $idhabitacion = $_POST['idhabitacion'];
+
+        $buscoFechaIngreso ="SELECT * FROM reserva INNER JOIN habitacion ON reserva.habitacion_idhabitacion = habitacion.idhabitacion WHERE reserva.fecha_ingreso = ? AND reserva.habitacion_idhabitacion = '".$idhabitacion."';";
+        $sentenciaFI = $bd->prepare($buscoFechaIngreso);
+        $sentenciaFI->execute(array($fecha_ingreso));
+        $resultadoFI = $sentenciaFI->fetch();
+
+        $buscoFechaSalida ="SELECT * FROM reserva INNER JOIN habitacion ON reserva.habitacion_idhabitacion = habitacion.idhabitacion WHERE reserva.fecha_ingreso = ? AND reserva.habitacion_idhabitacion = '".$idhabitacion."';";
+        $sentenciaFS = $bd->prepare($buscoFechaSalida);
+        $sentenciaFS->execute(array($fecha_salida));
+        $resultadoFS = $sentenciaFS->fetch();
+
+        if ($fecha_ingreso == "") {
+            $mensaje = "<script>document.getElementById('e_ingreso').innerHTML='Ingrese fecha.';</script>";
+            
+        }else if ($resultadoFI) {
+            $mensaje = "<script>document.getElementById('e_ingreso').innerHTML='Fecha no disponible.';</script>";
+
+        }else if ($fecha_salida == "") {
+            $mensaje = "<script>document.getElementById('e_salida').innerHTML='Ingrese fecha.';</script>";
+
+        }else if ($fecha_salida < $fecha_ingreso) {
+            $mensaje = "<script>document.getElementById('e_salida').innerHTML='La fecha de salida debe ser mayor a la fecha de ingreso.';</script>";
+
+        }
+    
+    } 
 
     if (isset($_POST["ajax"])) {
-       
+
         $nombre = $_POST['nombre'];
         $telefono = $_POST['telefono'];
         $email = $_POST['email'];
         $dirreccion = $_POST['dirreccion'];
         $provincia = $_POST['provincia'];
         $pais = $_POST['pais'];
-        
 
         if ($nombre == "") {
             $mensaje = "<script>document.getElementById('e_nombre').innerHTML='Por favor ingrese nombre.';</script>"; 
@@ -30,9 +68,6 @@
        
         }else if ($dirreccion == "") {
             $mensaje = "<script>document.getElementById('e_direccion').innerHTML='Por favor ingrese dirrección.';</script>"; 
-
-        }else if (!preg_match('/^\S+$/',$dirreccion)) {
-            $mensaje = "<script>document.getElementById('e_direccion').innerHTML='No ingrese espacios';</script>"; 
 
         }else if(!preg_match('/^[a-záéóóúàèìòùäëïöüñ\s]+$/i',$dirreccion)){
             $mensaje = "<script>document.getElementById('e_direccion').innerHTML='Solo se permiten letras!';</script>";
@@ -57,10 +92,24 @@
     
         }else{
 
-           
+            $sentenciaR = $bd->prepare("INSERT INTO reserva(fecha_ingreso,fecha_salida,habitacion_idhabitacion) VALUES (?,?,?);");
+            $resultadoR = $sentenciaR->execute([$fecha_ingreso,$fecha_salida,$idhabitacion]);
+            $idreserva = $bd->lastInsertId();
+
+            $sentencia = $bd->prepare("INSERT INTO cliente(nombre_completo,direccion,provincia,pais,telefono,email,reserva_idreserva) VALUES (?,?,?,?,?,?,?);");
+            $resultado= $sentencia->execute([$nombre,$dirreccion,$provincia,$pais,$telefono,$email,$idreserva]);
+            // $idCliente = $bd->lastInsertId();
+            $mensaje = "<script>window.location='index.php';</script>";
+          
         }
+
     }
 
+    
+
+    //echo $resultado;
+    //echo $idCliente;
+    //echo $idreserva;
     echo $mensaje;
 
 ?>
