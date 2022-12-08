92a9b945031a46e5d4f5fa9c547bd35aa235d438
= <=>
2022-12-08 12:50:50 -0300

    osq

--- admin/habitacion.php
+++ admin/habitacion.php
@@ -12,16 +12,13 @@
 </div>
 
 <!--estructura de la tabla-->
+<!--estructura de la tabla-->
 <div class="container">
-        <div class="row">
-            <div class="col-lg-12">            
-            <button id="btnNuevo" type="button" class="btn btn-info" data-toggle="modal">Nuevo</button>    
-            </div>    
-        </div>    
-    </div>    
+        
     <br>  
 
     <div class="container caja">
+         
         <div class="row">
             <div class="col-lg-12">
             <div class="table-responsive">        
@@ -33,30 +30,27 @@
                     <th scope="col">Nombre</th>
                     <th scope="col">Descripción</th>
                     <th scope="col">Imagen</th>
-                    <th scope="col">Precio</th>
-                    <th scope="col">Acciones</th>
+                    <th scope="col">precio</th>
+                    
+                    
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    foreach ($habitacion as $dato){ 
+                        foreach ($habitacion as $dato) {
                     ?>
                     <tr>
-                    <td scope="row"><?php echo $dato->idhabitacion;?></td>
-                    <td><?php echo $dato->tipo_habitacion;?></td>
-                    <td><?php echo $dato->descripcion;?></td>
-                    <td><?php echo $dato->img;?></td>
-                    <td><?php echo $dato->precio;?></td>
-                    <td>
-                        <a class="btn btn-primary btn-sm" href="">Editar</a>
-                        <a class="btn btn-danger btn-sm" href="">Eliminar</a>
-
-                    </td>
-                    </tr>
-                    <?php 
-                    }
-                    ?>
+                    <th scope="row"><?php echo $dato->idhabitacion ?></th>
+                    <td><?php echo $dato->tipo_habitacion ?></td>
+                    <td><?php echo $dato->descripcion ?></td>
                     
+                    <th><?php echo $dato->precio ?></th>
+                   
+                    
+                    </tr>
+                    <?php
+                        }
+                    ?>
                 </tbody>
 
                 </table>               
@@ -65,76 +59,4 @@
         </div>  
     </div>   
 
-<!--Modal para CRUD-->
-<div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        <form id="formcliente">    
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-lg-6">
-                    <div class="form-group">
-                    <label for="" class="col-form-label">Nombre:</label>
-                    <input type="text" class="form-control" id="nombre">
-                    </div>
-                    </div>
-                    <div class="col-lg-6">
-                    <div class="form-group">
-                    <label for="" class="col-form-label">Apellido</label>
-                    <input type="text" class="form-control" id="apellido">
-                    </div> 
-                    </div>    
-                </div>
-                <div class="row"> 
-                    <div class="col-lg-6">
-                    <div class="form-group">
-                    <label for="" class="col-form-label">Dirección</label>
-                    <input type="text" class="form-control" id="direccion">
-                    </div>               
-                    </div>
-                    <div class="col-lg-6">
-                    <div class="form-group">
-                    <label for="" class="col-form-label">Provincia</label>
-                    <input type="text" class="form-control" id="provincia">
-                    </div>
-                    </div>  
-                </div>
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="form-group">
-                        <label for="" class="col-form-label">Pais</label>
-                        <input type="text" class="form-control" id="pais">
-                        </div>
-                    </div>    
-                    <div class="col-lg-6">    
-                        <div class="form-group">
-                        <label for="" class="col-form-label">Teléfono</label>
-                        <input type="text" class="form-control" id="telefono">
-                        </div>            
-                    </div>    
-                </div>     
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="form-group">
-                        <label for="" class="col-form-label">E-mail</label>
-                        <input type="text" class="form-control" id="email">
-                        </div>
-                    </div>
-
-                </div>           
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
-                <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
-            </div>
-        </form>    
-        </div>
-    </div>
-</div>  
-      
 
