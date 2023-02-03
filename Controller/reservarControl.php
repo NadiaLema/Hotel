560795ba7b311477bdb32c6cc8f0720dba898abd
Maxi-Max10 <maxgiu3@gmail.com>
2023-02-03 11:53:45 -0300

    sh

--- Controller/reservarControl.php
+++ Controller/reservarControl.php
@@ -109,7 +109,6 @@
             $sentencia = $bd->prepare("INSERT INTO cliente(nombre_completo,direccion,provincia,pais,telefono,email,reserva_idreserva) VALUES (?,?,?,?,?,?,?);");
             $resultado= $sentencia->execute([$nombre,$dirreccion,$provincia,$pais,$telefono,$email,$idreserva]);
             // $idCliente = $bd->lastInsertId();
-<<<<<<< HEAD
 
             //Actualizo
             $resta = intval($datoCantOCP) + 1;
@@ -119,17 +118,6 @@
             $mensaje = "<script>window.location='index.php';</script>";
 
         } 
-=======
-            $mensaje = "<script>alert('Reserva Creada Exitosamente');
-                         window.location='index.php';
-                        </script>";
-            
-
-            
-        }
-        
-    
->>>>>>> b387d8aa71fdb4e4541c16c93bceca99840992c7
     } 
 
     echo $mensaje;
